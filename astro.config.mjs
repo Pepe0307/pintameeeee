b1bdf6f1ca1f151ace9bdce65a6fcd57275558e9
Nicolas Pereira <np.pepe@gmail.com>
2026-04-06 21:01:19 -0500

    PNT1 added supabase integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
+// Dummy comment to trigger deployment test
 export default defineConfig({
   site: 'https://your-art-gallery.com',
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'compile'
+  }),
   integrations: [sitemap()],
+  image: {
+    service: passthroughImageService(),
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'jxncyzrbjhoydsmjwsap.supabase.co',
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
